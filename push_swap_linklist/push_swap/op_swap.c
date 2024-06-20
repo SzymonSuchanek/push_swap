d51ef254981bfd0f027b4e2ba32a70dd363be531
SzymonSuchanek <jus20be20+1@gmail.com>
2024-06-20 19:13:15 +0200

    update

--- push_swap_linklist/push_swap/op_swap.c
+++ push_swap_linklist/push_swap/op_swap.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   op_swap.c                                          :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/01 14:08:29 by ssuchane          #+#    #+#             */
-/*   Updated: 2024/06/01 14:26:21 by ssuchane         ###   ########.fr       */
+/*   Created: 2024/06/17 15:02:18 by eaktimur          #+#    #+#             */
+/*   Updated: 2024/06/19 17:56:16 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_l **stack)
+void	s(t_l **stack, char which)
 {
 	t_l	*first;
 	t_l	*second;
@@ -28,32 +28,17 @@ void	sa(t_l **stack)
 	second->prev = first->prev;
 	first->prev = second;
 	*stack = second;
-	// update to ft_printf
-	printf("sa\n");
-}
-
-void	sb(t_l **stack)
-{
-	t_l	*first;
-	t_l	*second;
-
-	if (!stack || !*stack || !(*stack)->next)
-		return ;
-	first = *stack;
-	second = first->next;
-	first->next = second->next;
-	if (second->next)
-		second->next->prev = first;
-	second->next = first;
-	second->prev = first->prev;
-	first->prev = second;
-	*stack = second;
-	// update to ft_printf
-	printf("sb\n");
+	if (which == 'a')
+		write(1, "sa\n", 3);
+	else if (which == 'b')
+		write(1, "sb\n", 3);
+	update_indexes(NULL, stack);
 }
 
 void	ss(t_l **a, t_l **b)
 {
-	sa(a);
-	sa(b);
+	s(a, 'x');
+	s(b, 'x');
+	write(1, "ss\n", 3);
 }
+
